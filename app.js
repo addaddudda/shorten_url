04c1e3a006f34bcd39baaca8756f84c00b7a28a9
25-10171 <25-10171@edu3.gen.go.kr>
2025-04-30 23:45:47 +0900

    files

--- app.js
+++ app.js
@@ -4,3 +4,9 @@ const db = require('./db');
 app.listen(80, () => {
     console.log('http://localhost:80');
 })
+app.get('/', (req, res) => {
+    db.query('SELECT * FROM board', (err, results) => {
+        if(err) throw err;
+        res.send(results);
+    })
+})
