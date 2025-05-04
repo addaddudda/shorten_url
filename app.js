250f4fae6b65b55aa518a6a73e88406ef553f55a
addaddudda <addaddudda>
2025-05-05 01:00:39 +0900

    findurl부터 개발하기

--- app.js
+++ app.js
@@ -3,7 +3,13 @@ const app = express();
 const bodyparser = require('body-parser');
 const db = require('./db');
 const { nanoid } = require('nanoid');
+const session = require('express-session');
 app.set('view engine', 'pug');
+app.use(session({
+    secret: 'root', // 비밀 키 설정
+    resave: false,
+    saveUninitialized: true
+}));
 app.use(bodyparser.urlencoded({extended:false}));
 app.listen(80, () => {
     console.log('http://localhost:80');
@@ -24,7 +30,7 @@ app.post('/signin', (req, res) => {
         db.query('SELECT * FROM users WHERE id=?', [id], async (err, results) => {
             if(err) throw err;
             else if(results.length == 0){
-                db.query('INSERT INTO users (id, password ) VALUES (?, ?)', [id, password], (err, results) => {
+                db.query('INSERT INTO users (id, password) VALUES (?, ?)', [id, password], (err, results) => {
                     if(err) throw err;
                     res.send('완료!<p><a href="http://localhost:80/"> 메인으로 돌아가기 </a>');  
                })
@@ -44,6 +50,7 @@ app.post('/login', async (req, res) => {
     db.query('SELECT * FROM users WHERE id=? AND password=?', [id, password], (err, results) => {
         if(err) throw err;
         else if(results.length > 0){
+            req.session.user = results[0];
             res.redirect('http://localhost:80/');
         }
         else{
@@ -53,7 +60,11 @@ app.post('/login', async (req, res) => {
 })
 // shorten url먼저 개발
 app.get('/shorturl', (req, res) => {
-    res.render('shortenurl');
+    if(!req.session.user){
+        res.send('먼저 로그인 또는 회원가입을 해주세요! <p> <a href="http://localhost:80/login">로그인 주소</a> <p> <a href="http://localhost:80/signin">회원가입 주소</a>');
+    }else {
+        res.render('shortenurl');
+    }
 })
 app.post('/shorturl', (req, res) => {
     let url = req.body.url;
@@ -63,11 +74,17 @@ app.post('/shorturl', (req, res) => {
         res.send('줄이기 완료! <p>줄인 url: ' + shorturl + '<p><a href="http://localhost:80/">메인으로 돌아가기</a>');
     })
 })
-app.get('/:url', (req, res) => {
+app.get('/shorturl/:url', (req, res) => {
     let url = req.params.url;
     db.query('SELECT * FROM url WHERE shorturl=?', [url], (err, results) => {
         if(err) throw err;
-        let nomalurl = results[0].url;
-        res.redirect(nomalurl);
+        else if(results.length > 0){
+            res.redirect(results[0].url);
+        }else {
+            res.send('잘못된 url입니다!');
+        }
     })
+})
+app.get('/findurl', (req, res) => {
+    res.render('findurl');
 })
