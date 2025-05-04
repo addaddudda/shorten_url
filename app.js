f89cf6698ba4da50e9162ce4e92a0c5e231a03e8
addaddudda <hwon3330@naver.com>
2025-05-05 00:07:27 +0900

    url DB에 저장하기까지 완료

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const bodyparser = require('body-parser');
 const db = require('./db');
+const bcrypt = require('bcrypt');
+const nanoid = require('nanoid');
 app.set('view engine', 'pug');
 app.use(bodyparser.urlencoded({extended:false}));
 app.listen(80, () => {
@@ -20,7 +22,7 @@ app.post('/signin', (req, res) => {
     if(password != checkpassword){
         res.redirect('http://localhost:80/signin');
     }else{
-        db.query('SELECT * FROM users WHERE id=?', [id], (err, results) => {
+        db.query('SELECT * FROM users WHERE id=?', [id], async (err, results) => {
             if(err) throw err;
             else if(results.length == 0){
                 db.query('INSERT INTO users (id, password ) VALUES (?, ?)', [id, password], (err, results) => {
@@ -37,17 +39,31 @@ app.post('/signin', (req, res) => {
 app.get('/login', (req, res) => {
     res.render('login');
 })
-app.post('/login', (req, res) => {
+app.post('/login', async (req, res) => {
     let id = req.body.id;
     let password = req.body.password;
     db.query('SELECT * FROM users WHERE id=? AND password=?', [id, password], (err, results) => {
         if(err) throw err;
         else if(results.length > 0){
-            res.send('로그인 완료! <p> <a href="http://localhost:80/">메인으로 돌아가기기</a>')
+            res.redirect('http://localhost:80/');
         }
         else{
-            res.send('아이디 또는 비밀번호가 다릅니다 <p> <a href="http://localhost:80/login">로그인 화면으로 돌아가기</a>');
+            res.render('login2');
         }
     })
-}) //만들어야할것: 비밀번호 보기
+})
 // shorten url먼저 개발
+app.get('/shorturl', (req, res) => {
+    res.render('shortenurl');
+})
+app.post('/shorturl', (req, res) => {
+    let url = req.body.url;
+    let shorturl = nanoid(10);
+    db.query('INSERT INTO url (url, shorturl) VALUES (?, ?)' [url, shorturl], (err, results) => {
+        if(err) throw err;
+        res.send('줄이기 완료! <p><a href="http://localhost:80/">메인으로 돌아가기</a>');
+    })
+})
+app.get('/:url', (req, res) => {
+    
+})
